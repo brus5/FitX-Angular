438b534859d7c88a6a566978445f54c5c284bb8e
Łukasz Krawczak <lukasz634@gmail.com>
2019-09-25 13:29:38 +0200

    little changes

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 
 import {HomeComponent} from './components/home/home.component';
 import {BannerMainComponent} from './components/banner-main/banner-main.component';
+import {NewsComponent} from './components/news/news.component';
 
 import {SharedModule} from '../shared/shared.module';
 import {FooterModule} from '../footer/footer.module';
@@ -9,7 +10,8 @@ import {FooterModule} from '../footer/footer.module';
 @NgModule({
   declarations: [
     HomeComponent,
-    BannerMainComponent
+    BannerMainComponent,
+    NewsComponent
   ],
   imports: [
     SharedModule,
